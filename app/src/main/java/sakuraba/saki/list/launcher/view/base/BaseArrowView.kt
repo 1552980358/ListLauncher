4c70d3e92a4b6d1b93362a2aa72d2055f96db59b
1552980358 <1552980358@qq.com>
2021-06-22 20:33:55 +0800

    Update BaseArrowView.kt
    Signed-off-by: 1552980358 <1552980358@qq.com>

---
+++ app/src/main/java/sakuraba/saki/list/launcher/view/base/BaseArrowView.kt
@@ -0,0 +1,17 @@
+package sakuraba.saki.list.launcher.view.base
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.core.graphics.drawable.toBitmap
+import sakuraba.saki.list.launcher.R
+
+open class BaseArrowView: BaseView {
+    
+    constructor(context: Context): this(context, null)
+    constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
+    
+    protected var touchY = 0F
+    protected val pointer by lazy { resources.getDrawable(R.drawable.img_pointer, null).toBitmap() }
+    protected val pointerX by lazy { widthFloat - pointer.width }
+    
+}
