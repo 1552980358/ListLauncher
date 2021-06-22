21bf2414433f94d7491d8596096057b93af6a454
1552980358 <1552980358@qq.com>
2021-06-22 20:37:32 +0800

    Update ColorPresentView.kt
    Signed-off-by: 1552980358 <1552980358@qq.com>

---
+++ app/src/main/java/sakuraba/saki/list/launcher/view/ColorPresentView.kt
@@ -0,0 +1,47 @@
+package sakuraba.saki.list.launcher.view
+
+import android.content.Context
+import android.graphics.BitmapShader
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Shader
+import android.util.AttributeSet
+import androidx.annotation.ColorInt
+import androidx.core.graphics.drawable.toBitmap
+import sakuraba.saki.list.launcher.R
+import sakuraba.saki.list.launcher.view.base.BaseView
+
+class ColorPresentView: BaseView {
+    
+    constructor(context: Context): this(context, null)
+    constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
+    
+    private val bitmapShader by lazy { BitmapShader(resources.getDrawable(R.drawable.img_color_cube, null).toBitmap(), Shader.TileMode.REPEAT, Shader.TileMode.REPEAT) }
+    private val paintColor = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+    }
+    
+    init {
+        paint.shader = bitmapShader
+    }
+    
+    fun updateColor(@ColorInt color: Int) {
+        paintColor.color = color
+        invalidate()
+    }
+    
+    val color get() = paintColor.color
+    
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?:return
+        // Draw background cubes
+        // 绘制背景方格
+        canvas.drawRect(0F, 0F, widthFloat, heightFloat, paint)
+        
+        // Draw color
+        // 绘制颜色
+        canvas.drawRect(0F, 0F, widthFloat, heightFloat, paintColor)
+    }
+    
+}
