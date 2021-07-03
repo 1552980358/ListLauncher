47209f3a16882d83f93b9816e3194deb36d709ad
1552980358 <1552980358@qq.com>
2021-07-03 15:23:17 +0800

    Update SummaryTextView
    Signed-off-by: 1552980358 <1552980358@qq.com>

---
+++ app/src/main/java/sakuraba/saki/list/launcher/view/SummaryTextView.kt
@@ -0,0 +1,23 @@
+package sakuraba.saki.list.launcher.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import sakuraba.saki.list.launcher.view.base.TextViewInterface
+
+class SummaryTextView: AppCompatTextView {
+    
+    constructor(context: Context): this(context, null)
+    constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
+    
+    init {
+        getColorSync()
+    }
+    
+    fun getColorSync() {
+        if ((context as TextViewInterface).hasCustomSummaryTextColor()) {
+            setTextColor((context as TextViewInterface).getSummaryTextColor())
+        }
+    }
+    
+}
