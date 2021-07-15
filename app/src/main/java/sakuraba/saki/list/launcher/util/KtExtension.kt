7f8802feadc19bb88af9121d409489d0782de90e
1552980358 <1552980358@qq.com>
2021-07-15 18:54:29 +0800

    KtExtension.kt: Update Preference.updateIconColor()
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/sakuraba/saki/list/launcher/util/KtExtension.kt
+++ app/src/main/java/sakuraba/saki/list/launcher/util/KtExtension.kt
@@ -1,7 +1,13 @@
 package sakuraba.saki.list.launcher.util
 
 import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.preference.Preference
 
 val Bitmap.heightFloat: Float get() = height.toFloat()
 
 val Bitmap.widthFloat: Float get() = width.toFloat()
+
+fun Preference.updateIconColor(colorStr: String?) = icon?.setTint(Color.parseColor(colorStr))
+
+fun Preference.updateIconColor(color: Int) = icon?.setTint(color)
