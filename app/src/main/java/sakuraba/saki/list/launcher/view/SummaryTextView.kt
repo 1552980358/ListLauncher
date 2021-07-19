712a3f35ed84b3b44de9306432535870e353a322
1552980358 <1552980358@qq.com>
2021-07-19 18:56:44 +0800

    Rebase SummaryTextView and TitleTextView as SynchronizedTextView
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/sakuraba/saki/list/launcher/view/SummaryTextView.kt
+++ app/src/main/java/sakuraba/saki/list/launcher/view/SummaryTextView.kt
@@ -2,22 +2,16 @@ package sakuraba.saki.list.launcher.view
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatTextView
+import sakuraba.saki.list.launcher.view.base.SynchronizedTextView
 import sakuraba.saki.list.launcher.view.base.TextViewInterface
 
-class SummaryTextView: AppCompatTextView {
+class SummaryTextView: SynchronizedTextView {
     
     constructor(context: Context): this(context, null)
     constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
     
-    init {
-        getColorSync()
-    }
+    override fun hasSyncTextColor() = (context as TextViewInterface).isCustomSummaryColor
     
-    fun getColorSync() {
-        if ((context as TextViewInterface).isCustomSummaryColor) {
-            setTextColor((context as TextViewInterface).summaryColor)
-        }
-    }
+    override fun getSyncTextColor() = (context as TextViewInterface).summaryColor
     
 }
--- app/src/main/java/sakuraba/saki/list/launcher/view/TitleTextView.kt
+++ app/src/main/java/sakuraba/saki/list/launcher/view/TitleTextView.kt
@@ -2,22 +2,16 @@ package sakuraba.saki.list.launcher.view
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatTextView
+import sakuraba.saki.list.launcher.view.base.SynchronizedTextView
 import sakuraba.saki.list.launcher.view.base.TextViewInterface
 
-class TitleTextView: AppCompatTextView {
+class TitleTextView: SynchronizedTextView {
     
     constructor(context: Context): this(context, null)
     constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
     
-    init {
-        getColorSync()
-    }
+    override fun hasSyncTextColor() = (context as TextViewInterface).isCustomTitleColor
     
-    fun getColorSync() {
-        if ((context as TextViewInterface).isCustomTitleColor) {
-            setTextColor((context as TextViewInterface).titleColor)
-        }
-    }
+    override fun getSyncTextColor() = (context as TextViewInterface).titleColor
     
 }
