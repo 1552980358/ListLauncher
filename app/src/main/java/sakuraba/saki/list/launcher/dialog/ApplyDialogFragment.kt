7499b9d0a3c4cd1b6528601d4b64f7b356e6e24c
1552980358 <1552980358@qq.com>
2021-07-22 18:54:10 +0800

    ApplyDialogFragment: Update constructor
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/sakuraba/saki/list/launcher/dialog/ApplyDialogFragment.kt
+++ app/src/main/java/sakuraba/saki/list/launcher/dialog/ApplyDialogFragment.kt
@@ -2,12 +2,16 @@ package sakuraba.saki.list.launcher.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import sakuraba.saki.list.launcher.R
 
-class ApplyDialogFragment(private val listener: OnApplyListener): DialogFragment() {
+class ApplyDialogFragment(
+    private val listener: OnApplyListener,
+    @StringRes private val message: Int = R.string.apply_dialog_message,
+    @StringRes private val positiveButton: Int = R.string.apply_dialog_apply): DialogFragment() {
     
     companion object {
         interface OnApplyListener {
@@ -20,8 +24,8 @@ class ApplyDialogFragment(private val listener: OnApplyListener): DialogFragment
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(requireContext())
             .setTitle(R.string.apply_dialog_title)
-            .setMessage(R.string.apply_dialog_message)
-            .setPositiveButton(R.string.apply_dialog_apply) { _, _ -> listener.onApply() }
+            .setMessage(message)
+            .setPositiveButton(positiveButton) { _, _ -> listener.onApply() }
             .setNegativeButton(R.string.apply_dialog_cancel) { _, _ -> listener.onCancel() }
             .create()
     }
