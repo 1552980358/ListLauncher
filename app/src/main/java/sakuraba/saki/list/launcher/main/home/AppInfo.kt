0b90623ab9c24a473d145cd70a2b830a24656a56
1552980358 <1552980358@qq.com>
2021-07-28 20:16:16 +0800

    AppInfo.kt: Inherit to Serializable
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- app/src/main/java/sakuraba/saki/list/launcher/main/home/AppInfo.kt
+++ app/src/main/java/sakuraba/saki/list/launcher/main/home/AppInfo.kt
@@ -1,7 +1,8 @@
 package sakuraba.saki.list.launcher.main.home
 
 import android.graphics.drawable.Drawable
+import java.io.Serializable
 
-class AppInfo(val name: String, val packageName: String, val icon: Drawable, val isSystem: Boolean, val versionName: String, val versionCode: Long) {
+class AppInfo(val name: String, val packageName: String, val icon: Drawable, val isSystem: Boolean, val versionName: String, val versionCode: Long): Serializable {
     var pinYin = ""
 }
